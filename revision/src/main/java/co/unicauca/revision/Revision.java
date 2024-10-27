b07f5802754aee90804c342e181af54105b091ad
Andrea C. Realpe <110572707+andrearealpe@users.noreply.github.com>
2024-10-27 04:09:39 -0500

    inicio revision

---
+++ revision/src/main/java/co/unicauca/revision/Revision.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.unicauca.revision;
+
+/**
+ *
+ * @author earea
+ */
+public class Revision {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
